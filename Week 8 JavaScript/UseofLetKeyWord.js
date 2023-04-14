822de88a98357e56a0fe912415f212112bafb1a4
Jagdev Singh Dosanjh <jagdevsinghdosanjh@gmail.com>
2023-04-14 17:44:19 +0530

    OK

--- Week 8 JavaScript/UseofLetKeyWord.js
+++ Week 8 JavaScript/UseofLetKeyWord.js
@@ -1,7 +1,8 @@
 let x=1 
 //Use of let keyword does not allow to declare one variable again with different values.
 console.log(x)
-let x1=1
+let x1=2
 console.log(x1)
 //Even if we try to declare same variable (declared with let keyword) using var it will through an error.
-let x
+let x2=3
+console.log(x2)
