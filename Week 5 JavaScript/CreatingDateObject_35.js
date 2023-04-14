ed2ef7b95c43c8f6fe77f8c59f7a26da9fd7cfc0
Jagdev Singh Dosanjh <jagdevsinghdosanjh@gmail.com>
2023-04-14 14:26:26 +0530

    OK

---
+++ Week 5 JavaScript/CreatingDateObject_35.js
@@ -0,0 +1,18 @@
+var today=new Date();
+console.log(today);
+var milleniumStart=new Date(2000,0,1);
+console.log(milleniumStart);
+
+today.setDate(today.getDate()+1);
+console.log(today);
+
+var July31=new Date(2000, 6,31,5);
+console.log(July31);
+
+July31.setDate(July31.getDate()+1);
+console.log(July31)
+
+const date1=new Date(2020,1,1);
+
+console.log(date1);
+
